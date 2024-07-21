8b3e7979178a9cf3b148846b7d75958c48dcaf11
Umang Barewar <117373781+UmangBarewar@users.noreply.github.com>
2024-07-22 01:30:53 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = 'https://res.cloudinary.com/du6yeb9y3/image/upload/
+MEDIA_URL = 'https://res.cloudinary.com/du6yeb9y3/image/upload/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # this above line is being removed for giving cloud access to cloudinary
 
